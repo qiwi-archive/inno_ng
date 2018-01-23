c55f6a238f4c18ac7c50882bf0006191e3e32f95
Nikita Khromushkin <n.khromushkin@qiwi.com>
2018-01-23 17:16:27 +0300

    jwtHttpService.processOptions fix

--- src/http/jwt-http.service.ts
+++ src/http/jwt-http.service.ts
@@ -9,17 +9,17 @@ export class JwtHttpService extends HttpService {
     }
 
     async request<T>(method: string, url: string, options: IRequestOptions): Promise<T> {
-        options = this.processOptions(options);
+        this.processOptions(options);
         return super.request<T>(method, url, options);
     }
 
     async requestRaw<T>(method: string, url: string, options: IRequestOptions): Promise<Blob> {
-        options = this.processOptions(options);
+        this.processOptions(options);
         return super.requestRaw<T>(method, url, options);
     }
 
     processOptions(options: IRequestOptions): void {
-        options.headers = options.headers || new HttpHeaders();
+        options.headers = options.headers || {};
         if (this.authStorageService.isAuthenticated()) {
             options.headers['Authorization'] = 'Bearer ' + this.authStorageService.getCurrentJwtToken();
         }
